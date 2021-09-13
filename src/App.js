f3f19831318adcaf7dd9134468b4b76203c61694
Shoaib Riaz <sriaz@jobcase.com>
2021-09-14 01:28:27 +0500

    updating docs and testing on android

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StatusBar, useWindowDimensions} from 'react-native';
+import {StatusBar, useWindowDimensions, View} from 'react-native';
 import * as theme from './theme';
 import {ThemeProvider} from 'styled-components';
 import {APP_COLORS} from './utils/constants';
 import {Home} from './screens/home';
 
 const App = () => {
+  // return <View style={{height: 50, width:'100%', backgroundColor:'red'}}/>
   const {width: windowWidth, height: windowHeight} = useWindowDimensions();
   return (
     <ThemeProvider theme={{...theme, windowWidth, windowHeight}}>
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const SERVER_BASE_URL = 'https://api.flickr.com/services/rest/';
+//Write your API key here;
+export const API_KEY = 'TEST_API_KEY';
 export const API_URL = `${SERVER_BASE_URL}?method=flickr.photos.search&api_key=${API_KEY}&format=json&nojsoncallback=1&per_page=26&page=`;
-//Write your API key here and uncomment the line;
-// export const API_KEY = 
